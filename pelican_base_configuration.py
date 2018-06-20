6a9a1eeaedc7baa757bac4fb12c1e7a99f3aea18
Yuval Langer <yuval.langer@gmail.com>
2018-06-20 03:28:13 +0300

    Switch to simple theme and add display pages and categories options

--- pelican_base_configuration.py
+++ pelican_base_configuration.py
@@ -1,6 +1,10 @@
 TIMEZONE = "UTC"
 PATH = "content/"
 SITENAME = "Kaka Farm"
+THEME = "simple"
+
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
 
 TAG_FEED_ATOM = "feeds/tags/%s.atom.xml"
 TAG_FEED_RSS = "feeds/tags/%s.rss.xml"
